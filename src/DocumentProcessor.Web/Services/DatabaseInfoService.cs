c3f3758ebb597baa94e7db84f8b72638342ad5c4
Ty Augustine <taugustj@amazon.com>
2025-12-02 11:33:54 -0800

    reset

--- src/DocumentProcessor.Web/Services/DatabaseInfoService.cs
+++
@@ -1,8 +0,0 @@
-namespace DocumentProcessor.Web.Services;
-
-public class DatabaseInfoService
-{
-    public string DatabaseType { get; set; } = "Unknown";
-    public string SecretName { get; set; } = "None";
-    public string HostAddress { get; set; } = "Unknown";
-}
